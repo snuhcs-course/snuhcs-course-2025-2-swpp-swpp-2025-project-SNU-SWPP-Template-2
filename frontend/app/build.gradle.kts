e1311e17f95522154171be68b2f926f6b4f39722
vnny27 <dalitravan@gmail.com>
2025-09-27 17:04:56 +0900

    feat: structure refine

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
