0311f17e89c02d26fb46e631a1bab97f9132de05
kangaroot45 <kangaroot45@snu.ac.kr>
2025-11-03 14:13:20 +0900

    fix: django engine for postgis

--- server/config/settings.py
+++ server/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'users',
     'restaurant',
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'foodigram',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
