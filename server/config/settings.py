b9c536ab2d2d6870ffe56082ccb6e12308fd93cb
dakann1218 <dakann1218@snu.ac.kr>
2025-11-04 16:19:43 +0900

    chore: CSRF_TRUSTED_ORIGINS

--- server/config/settings.py
+++ server/config/settings.py
@@ -150,6 +150,9 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'http://10.0.2.2:8000',
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
+    'http://10.0.2.2:8001',
 ]
 
 # Logging configuration
