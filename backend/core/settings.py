a159d37cff46d58f94bd356723dd92f1a46d3226
Butemj Bat-Orshikh <butemjb@Butemjs-MacBook-Pro.local>
2025-11-16 20:47:02 +0900

    Minor fix. Ignore

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,7 @@ DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "localhost,127.0.0.1,10.0.2.2,192.0.0.2,testserver,172.30.1.16",
+    "localhost,127.0.0.1,10.0.2.2,192.0.0.2,testserver,15.165.200.135:8000",
 ).split(",")
 # Allow all hosts in 192.0.0.x network for development
 if DEBUG:
